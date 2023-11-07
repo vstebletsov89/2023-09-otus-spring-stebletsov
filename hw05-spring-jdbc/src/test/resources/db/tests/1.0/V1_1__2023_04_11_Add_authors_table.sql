6dc24381f316db735d136da3ed2554c328336621
Vyacheslav Stebletsov <vyacheslav.stebletsov@broadcom.com>
2023-11-07 18:12:57 +0100

    code review comments

--- hw05-spring-jdbc/src/test/resources/db/tests/1.0/V1_1__2023_04_11_Add_authors_table.sql
+++
@@ -1,8 +0,0 @@
---date: 2023-04-11
---author: vstebletsov
-
-create table authors (
-     id bigserial,
-     full_name varchar(255),
-     primary key (id)
-)
--- hw05-spring-jdbc/src/test/resources/db/tests/1.0/V1_2__2023_04_11_Add_genres_table.sql
+++
@@ -1,8 +0,0 @@
---date: 2023-04-11
---author: vstebletsov
-
-create table genres (
-    id bigserial,
-    name varchar(255),
-    primary key (id)
-)
