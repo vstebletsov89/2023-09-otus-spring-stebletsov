e1a2757c30762614130a81b451ddcd4e2cde1c74
Vyacheslav Stebletsov <vyacheslav.stebletsov@broadcom.com>
2023-11-14 16:01:30 +0100

    fix checkstyle

--- hw06-spring-orm/src/main/java/ru/otus/hw/models/Book.java
+++ hw06-spring-orm/src/main/java/ru/otus/hw/models/Book.java
@@ -8,6 +8,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedAttributeNode;
+import jakarta.persistence.NamedEntityGraph;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,6 +20,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "books")
+@NamedEntityGraph(
+        name = "book-entity-graph",
+        attributeNodes = {
+                @NamedAttributeNode("author"),
+                @NamedAttributeNode("genre")
+        }
+)
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- hw06-spring-orm/src/main/java/ru/otus/hw/repositories/BookRepositoryJpa.java
+++ hw06-spring-orm/src/main/java/ru/otus/hw/repositories/BookRepositoryJpa.java
@@ -1,9 +1,11 @@
 package ru.otus.hw.repositories;
 
+import jakarta.persistence.EntityGraph;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
+import static org.springframework.data.jpa.repository.EntityGraph.EntityGraphType.FETCH;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,6 +34,9 @@ public class BookRepositoryJpa implements BookRepository {
     public List<Book> findAll() {
         TypedQuery<Book> query =
                 em.createQuery("select b from Book b", Book.class);
+        EntityGraph<?> entityGraph =
+                em.getEntityGraph("book-entity-graph");
+        query.setHint(FETCH.getKey(), entityGraph);
         return query.getResultList();
     }
 
