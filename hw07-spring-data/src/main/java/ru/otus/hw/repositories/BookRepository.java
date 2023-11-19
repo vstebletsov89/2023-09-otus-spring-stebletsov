55335bc1c3543f8ce3d39dd8cdef33da8b629b44
vstebletsov89 <vstebletsov89@github.com>
2023-11-19 12:45:28 +0100

    homework done

--- hw07-spring-data/src/main/java/ru/otus/hw/repositories/BookRepository.java
+++ hw07-spring-data/src/main/java/ru/otus/hw/repositories/BookRepository.java
@@ -1,5 +1,6 @@
 package ru.otus.hw.repositories;
 
+import jakarta.annotation.Nonnull;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.hw.models.Book;
@@ -8,6 +9,7 @@ import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+    @Nonnull
     @EntityGraph(attributePaths = {"author", "genre"})
     @Override
     List<Book> findAll();
