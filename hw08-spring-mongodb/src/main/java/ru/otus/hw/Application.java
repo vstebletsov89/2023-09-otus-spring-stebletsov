0a8a7818784f1a476708c1a926f057f26b125e53
vstebletsov89 <vstebletsov89@github.com>
2023-12-02 17:59:12 +0100

    wip

--- hw08-spring-mongodb/src/main/java/ru/otus/hw/Application.java
+++ hw08-spring-mongodb/src/main/java/ru/otus/hw/Application.java
@@ -3,10 +3,10 @@ package ru.otus.hw;
 import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongock
-@EnableConfigurationProperties
+@EnableMongoRepositories
 @SpringBootApplication
 public class Application {
 
