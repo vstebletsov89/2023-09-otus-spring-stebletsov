a12817c0f319947347ea3f9a711d0189dbbb24d4
Vyacheslav Stebletsov <vyacheslav.stebletsov@broadcom.com>
2024-01-12 17:11:51 +0100

    wip

--- hw12-spring-security/src/test/java/ru/otus/hw/controllers/RestExceptionHandlerTest.java
+++ hw12-spring-security/src/test/java/ru/otus/hw/controllers/RestExceptionHandlerTest.java
@@ -4,36 +4,47 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.otus.hw.dto.BookDto;
+import ru.otus.hw.dto.BookCreateDto;
+import ru.otus.hw.mappers.AuthorMapperImpl;
+import ru.otus.hw.mappers.BookMapperImpl;
+import ru.otus.hw.mappers.GenreMapperImpl;
+import ru.otus.hw.models.Author;
 import ru.otus.hw.repositories.AuthorRepository;
 import ru.otus.hw.repositories.BookRepository;
 import ru.otus.hw.repositories.GenreRepository;
-import ru.otus.hw.services.AuthorService;
 import ru.otus.hw.services.BookService;
+import ru.otus.hw.services.BookServiceImpl;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("Проверка работы глобального обработчика ошибок")
-@WebMvcTest(controllers = {
+@SpringBootTest(classes = {
+        BookController.class,
+        BookServiceImpl.class,
         RestExceptionHandler.class,
-        BookController.class
-},
-        includeFilters = {})
+        BookMapperImpl.class,
+        AuthorMapperImpl.class,
+        GenreMapperImpl.class
+})
+@AutoConfigureWebMvc
+@AutoConfigureMockMvc
 class RestExceptionHandlerTest {
 
     @Autowired
@@ -64,83 +75,67 @@ class RestExceptionHandlerTest {
     @Test
     void shouldHandleNotFoundExceptionForBook() throws Exception {
         doReturn(Optional.empty()).when(bookRepository).findById(anyLong());
-        var response = "ffff";
 
         mockMvc.perform(get("/api/v1/books/{id}", 999))
                 .andDo(print())
                 .andExpect(status().isNotFound())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(objectMapper.writeValueAsString(response)));
+                .andExpect(content().contentType(MediaType.valueOf("text/plain;charset=UTF-8")))
+                .andExpect(content().string("Book with id 999 not found"));
 
-        assertEquals("Book with id 999 not found", response);
         verify(bookRepository, times((1))).findById(anyLong());
     }
-//
-//    @DisplayName("должен обрабатывать исключение, когда автор не найден")
-//    @Test
-//    void shouldHandleNotFoundExceptionForAuthor() {
-//        doReturn(Optional.empty()).when(authorRepository).findById(anyString());
-//        doReturn(Optional.empty()).when(genreRepository).findById(anyString());
-//        WebTestClient testClient = WebTestClient.bindToController(bookController)
-//                .controllerAdvice(restExceptionHandler)
-//                .build();
-//
-//        var response = testClient.post()
-//                .uri("/api/v1/books")
-//                .body(BodyInserters.fromValue(new BookCreateDto("newTitle", "999", "999")))
-//                .accept(MediaType.APPLICATION_JSON)
-//                .exchange()
-//                .expectStatus()
-//                .isNotFound()
-//                .expectBody(String.class)
-//                .returnResult()
-//                .getResponseBody();
-//
-//        assertEquals("Author with id 999 not found", response);
-//        verify(authorRepository, times((1))).findById(anyString());
-//    }
-//
-//    @DisplayName("должен обрабатывать исключение, когда жанр не найден")
-//    @Test
-//    void shouldHandleNotFoundExceptionForGenre() {
-//        doReturn(Mono.just(new Author())).when(authorRepository).findById(anyString());
-//        doReturn(Mono.empty()).when(genreRepository).findById(anyString());
-//        WebTestClient testClient = WebTestClient.bindToController(bookController)
-//                .controllerAdvice(restExceptionHandler)
-//                .build();
-//
-//        var response = testClient.post()
-//                .uri("/api/v1/books")
-//                .body(BodyInserters.fromValue(new BookCreateDto("newTitle", "999", "999")))
-//                .accept(MediaType.APPLICATION_JSON)
-//                .exchange()
-//                .expectStatus()
-//                .isNotFound()
-//                .expectBody(String.class)
-//                .returnResult()
-//                .getResponseBody();
-//
-//        assertEquals("Genre with id 999 not found", response);
-//        verify(genreRepository, times((1))).findById(anyString());
-//    }
-//
-//    @DisplayName("должен обрабатывать исключени внутренней ошибки сервера")
-//    @Test
-//    void shouldHandleAnyRuntimeException() {
-//        WebTestClient testClient = WebTestClient.bindToController(bookController)
-//                .controllerAdvice(restExceptionHandler)
-//                .build();
-//
-//        var response = testClient.get()
-//                .uri("/invalid_uri")
-//                .accept(MediaType.APPLICATION_JSON)
-//                .exchange()
-//                .expectStatus()
-//                .is5xxServerError()
-//                .expectBody(String.class)
-//                .returnResult()
-//                .getResponseBody();
-//
-//        assertThat(response).contains("org.springframework.web.server.ResponseStatusException: 404 NOT_FOUND");
-//    }
+
+    @DisplayName("должен обрабатывать исключение, когда автор не найден")
+    @Test
+    void shouldHandleNotFoundExceptionForAuthor() throws Exception {
+        doReturn(Optional.empty()).when(authorRepository).findById(anyLong());
+        doReturn(Optional.empty()).when(genreRepository).findById(anyLong());
+
+        mockMvc.perform(post("/api/v1/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new BookCreateDto("newTitle",
+                                        999L,
+                                        999L))))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.valueOf("text/plain;charset=UTF-8")))
+                .andExpect(content().string("Author with id 999 not found"));
+
+        verify(authorRepository, times((1))).findById(anyLong());
+    }
+
+    @DisplayName("должен обрабатывать исключение, когда жанр не найден")
+    @Test
+    void shouldHandleNotFoundExceptionForGenre() throws Exception {
+        doReturn(Optional.of(new Author())).when(authorRepository).findById(anyLong());
+        doReturn(Optional.empty()).when(genreRepository).findById(anyLong());
+
+        mockMvc.perform(post("/api/v1/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new BookCreateDto("newTitle",
+                                        999L,
+                                        999L))))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.valueOf("text/plain;charset=UTF-8")))
+                .andExpect(content().string("Genre with id 999 not found"));
+
+        verify(authorRepository, times((1))).findById(anyLong());
+    }
+
+    @DisplayName("должен обрабатывать исключени внутренней ошибки сервера")
+    @Test
+    void shouldHandleAnyRuntimeException() throws Exception {
+        doReturn(Optional.ofNullable(new RuntimeException())).when(bookRepository).findById(anyLong());
+
+        mockMvc.perform(get("/api/v1/books/{id}", 999))
+                .andDo(print())
+                .andExpect(status().isInternalServerError())
+                .andExpect(content().contentType(MediaType.valueOf("text/plain;charset=UTF-8")))
+                .andExpect(content()
+                        .string(containsString("class java.lang.RuntimeException")));
+
+    }
 }
