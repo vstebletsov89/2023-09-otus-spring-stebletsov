32ac3441d15c355a66b23e7bebf7503f5efd3475
Vyacheslav Stebletsov <vyacheslav.stebletsov@broadcom.com>
2024-02-07 17:34:42 +0100

    wip

--- hw16-spring-boot-actuator/src/main/java/ru/otus/hw/repositories/BookRepository.java
+++ hw16-spring-boot-actuator/src/main/java/ru/otus/hw/repositories/BookRepository.java
@@ -2,11 +2,13 @@ package ru.otus.hw.repositories;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.otus.hw.models.Book;
 
 import java.util.List;
 import java.util.Optional;
 
+@RepositoryRestResource(path = "book")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     @EntityGraph(value = "book-entity-graph")
--- hw16-spring-boot-actuator/src/main/java/ru/otus/hw/repositories/UserRepository.java
+++ hw16-spring-boot-actuator/src/main/java/ru/otus/hw/repositories/UserRepository.java
@@ -1,10 +1,12 @@
 package ru.otus.hw.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.otus.hw.models.User;
 
 import java.util.Optional;
 
+@RepositoryRestResource(path = "user")
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
